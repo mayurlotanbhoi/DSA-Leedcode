1595ac79ef84c435808dccedad42cff3eb382b02
mayur bhoi <mayurbhoi200@gmail.com>
2026-08-15 15:47:03 +0530

    Time: 4 ms (100%), Space: 62.5 MB (21.43%) - LeetHub

---
+++ 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var largestMerge = function(word1, word2) {
+
+    let merge = ""
+
+    while(word1.length && word2.length){
+       
+       if(word1 > word2){
+        merge += word1[0]
+        word1 = word1.slice(1)
+       }else{
+        merge += word2[0]
+        word2 = word2.slice(1)
+       }
+    }
+
+    merge += word1;
+    merge += word2;
+
+    return merge
+    
+};
