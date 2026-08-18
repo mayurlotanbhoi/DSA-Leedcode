9f3dbbeb0f89449ac7cfcfde484f9385a61b179c
mayur bhoi <mayurbhoi200@gmail.com>
2026-08-18 08:59:03 +0530

    Time: 1 ms (73.44%), Space: 56.2 MB (57.82%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+
+    let ans = 0
+
+  for(const num of nums){
+    ans ^= num
+  } 
+  return ans   
+};
