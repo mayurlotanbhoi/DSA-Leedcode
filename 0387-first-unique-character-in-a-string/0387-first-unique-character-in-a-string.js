51132ebf4a8d4c7bd88bd21426edd306eaa0beb4
mayur bhoi <mayurbhoi200@gmail.com>
2026-08-22 13:31:27 +0530

    Time: 28 ms (81%), Space: 59.3 MB (70.68%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    const map = {}
+    for(let i=0; i<s.length; i++){
+        map[s[i]] = (map[s[i]] || 0 ) +1
+    }
+    for(let i=0; i<s.length; i++){
+        if(map[s[i]] == 1){
+            return i
+        }
+    }
+    return -1
+};
