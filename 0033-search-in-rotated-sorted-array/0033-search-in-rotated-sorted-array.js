93d5ee140fca0b624c9b9bf83b0f5b7463b0d7d4
mayur bhoi <mayurbhoi200@gmail.com>
2026-09-03 19:59:47 +0530

    Time: 0 ms (100%), Space: 52.1 MB (98.4%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -5,34 +5,36 @@
  */
 var search = function(nums, target) {
 
-    let left =0
-    let right = nums.length-1
+    let left = 0
+    let right = nums.length -1
 
-    while(left <= right){
+    while( left <= right){
 
         const mid = Math.floor((left+right)/2)
 
         if(nums[mid] == target) return mid
 
         if(nums[left] <= nums[mid]){
-             // Target lies inside sorted left half
-            if (nums[left] <= target && target < nums[mid]) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+            // left part is sorted
+            if(target  >= nums[left] &&  target < nums[mid]){
+                 right = mid-1
+            }else{
+                left = mid +1
             }
-            // leftside
+
         }else{
 
-            // Target lies inside sorted right half
-            if (nums[mid] < target && target <= nums[right]) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+            if(target > nums[mid] && target <= nums[right] ){
+                 left = mid +1
+            }else{
+                right = mid-1
             }
-        }
-    }
 
-    return -1;
+        }
+
+
+
+    }
     
+    return -1
 };
